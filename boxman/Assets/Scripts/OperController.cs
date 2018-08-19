3ee0c6be4baf1aab6fdea8486c262f2194c80533
zlk <849693228@qq.com>
2018-08-20 00:12:59 +0800

    first commit

---
+++ boxman/Assets/Scripts/OperController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OperController : MonoBehaviour {
+
+    Button mLeftBtn;
+    Button mTopBtn;
+    Button mRightBtn;
+    Button mBottomBtn;
+    Button mPreBtn;
+    Button mRestartBtn;
+
+	// Use this for initialization
+	void Start () {
+        mLeftBtn = transform.Find("left").GetComponent<Button>();
+
+        mLeftBtn.onClick.AddListener(OnLeftClick);
+	}
+	
+    void OnLeftClick(){
+        Debug.Log("left click ---");
+    }
+
+
+}
